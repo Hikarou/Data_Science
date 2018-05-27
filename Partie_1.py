15ccd0ba4ee2db1b707e1e74629a74d221bd98c3
José Pinto <jose.ferropinto@epfl.ch>
2018-05-27 12:18:55 +0200

    UPDATE: CTRL + ALT + L

--- Partie_1.py
+++ Partie_1.py
@@ -3,27 +3,24 @@ import numpy as np
 
 titanic_data = np.loadtxt('titanic.dat', delimiter=',', skiprows=8)
 
-means = []#mean value for each K
+means = []  # mean value for each K
 
-for i in range(1, 20):#each K
+for i in range(1, 20):  # each K
     KM = KMeans(n_clusters=i, random_state=0).fit(titanic_data)
-    
+
     labels = KM.labels_
     clusters_distances = KM.transform(titanic_data)
-    mean_distances = []#mean distances for each cluster and its points
-    
-    for j in range(i):#index of each cluster
+    mean_distances = []  # mean distances for each cluster and its points
+
+    for j in range(i):  # index of each cluster
         n_points = 0
         result = 0
-        for k in range(len(titanic_data)):#index of each data point
+        for k in range(len(titanic_data)):  # index of each data point
             if (labels[k] == j):
-                n_points+=1
+                n_points += 1
                 result += clusters_distances[k][j]
-        mean_distances.append(result/n_points)
-    
+        mean_distances.append(result / n_points)
+
     means.append(np.mean(mean_distances))
-    
-      
-print(means)         
-         
-        
+
+print(means)
