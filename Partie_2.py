43ac4b84fb6707eda9b899d99a9e35ceb9e7f614
José Pinto <jose.ferropinto@epfl.ch>
2018-05-28 17:47:09 +0200

    UPDATE: Work with all the datas

--- Partie_2.py
+++ Partie_2.py
@@ -11,36 +11,34 @@ import numpy as np
 
 
 def main() -> None:
-    breast_cancer = datasets.load_breast_cancer()
-    breast_data = preprocessing.normalize(breast_cancer['data'])
-    breast_target = breast_cancer['target']
-
-    print(breast_cancer)
-
-    wine = datasets.load_wine()
-    wine_data = wine['data']
-    wine_target = wine['target']
+    datas = [datasets.load_breast_cancer, datasets.load_wine]
 
     kf = RepeatedKFold(n_splits=5, n_repeats=10, random_state=None)
 
-    n_neigh_scores = []
-    for n_neighbors in range(1, 11):
-        local_scores = []
-        for train_index, test_index in kf.split(breast_data):
-            # print("Train:", train_index, "Validation:", test_index)
-            X_train, X_test = breast_data[train_index], breast_data[test_index]
-            y_train, y_test = breast_target[train_index], breast_target[test_index]
+    for data in datas:
+        raw = data()
+        print(raw['DESCR'].split(' ', 1)[0], "data :")
+        X = preprocessing.normalize(raw['data'])
+        y = raw['target']
 
-            k_neigh = KNeighborsClassifier(n_neighbors=n_neighbors)
-            k_neigh.fit(X_train, y_train)
+        n_neigh_scores = []
+        for n_neighbors in range(1, 11):
+            local_scores = []
+            for train_index, test_index in kf.split(X):
+                # print("Train:", train_index, "Validation:", test_index)
+                X_train, X_test = X[train_index], X[test_index]
+                y_train, y_test = y[train_index], y[test_index]
 
-            predictions = k_neigh.predict(X_test)
-            local_scores.append(accuracy_score(y_test, predictions))
-        print(local_scores)
-        n_neigh_scores.append(np.mean(np.array(local_scores)))
-    print(n_neigh_scores)
-    n_neigh_mean = np.mean(np.array(n_neigh_scores))
-    print("n_neigh mean : {}".format(n_neigh_mean))
+                k_neigh = KNeighborsClassifier(n_neighbors=n_neighbors)
+                k_neigh.fit(X_train, y_train)
+
+                predictions = k_neigh.predict(X_test)
+                local_scores.append(accuracy_score(y_test, predictions))
+            # print(local_scores)
+            n_neigh_scores.append(np.mean(np.array(local_scores)))
+        # print(n_neigh_scores)
+        n_neigh_mean = np.mean(np.array(n_neigh_scores))
+        print("n_neigh mean : {}".format(n_neigh_mean))
 
     pass
 
