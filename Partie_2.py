47a5d77fffc5bc926bc2340734a54cd7c932de45
José Pinto <jose.ferropinto@epfl.ch>
2018-05-28 20:14:52 +0200

    UPDATE : MLP and part 2 finished

--- Partie_2.py
+++ Partie_2.py
@@ -14,28 +14,57 @@ def main() -> None:
     datas = [datasets.load_breast_cancer, datasets.load_wine]
     datas_name = ["Breast Cancer", "Wine"]
 
-    classifiers = []
+    classifiers = [KNeighborsClassifier, DecisionTreeClassifier, MLPClassifier, MLPClassifier, MLPClassifier,
+                   MLPClassifier]
 
-    # KNeighborsClassifier part
-    sub_classifiers = []
+    params = []
+
+    # KNeighborsClassifier
+    sub_params = []
     for n_neighbors in range(1, 52, 5):
-        sub_classifiers.append(KNeighborsClassifier(n_neighbors=n_neighbors))
-    classifiers.append(sub_classifiers)
+        sub_params.append({"n_neighbors": n_neighbors})
+    params.append(sub_params)
 
     # DecisionTreeClassifier
-    sub_classifiers = []
-    for min_samples_leaf in range(1, 52, 5):
-        sub_classifiers.append(DecisionTreeClassifier(min_samples_leaf=min_samples_leaf))
-    classifiers.append(sub_classifiers)
+    sub_params = []
+    for min_samples_leaf in range(2, 53, 5):
+        sub_params.append({"min_samples_leaf": min_samples_leaf})
+    params.append(sub_params)
 
-    # MLP
-    classifiers.append([MLPClassifier(solver='sgd', activation='logistic', max_iter=1000, hidden_layer_sizes=(5, 5),
-                                      verbose=False, learning_rate_init=0.1, tol=0., early_stopping=True)])
-    classifiers.append([MLPClassifier(solver='adam', activation='logistic', max_iter=1000, hidden_layer_sizes=(5, 5),
-                                      verbose=False, learning_rate_init=0.1, tol=0., early_stopping=True)])
+    # MLPClassifier one layer sgd_solver
+    sub_params = []
+    for nodes in range(2, 20, 3):
+        sub_params.append({"solver": 'sgd', "activation": 'logistic', "max_iter": 1000, "hidden_layer_sizes": (nodes,),
+                           "verbose": False, "learning_rate_init": 0.1, "tol": 0., "early_stopping": True})
+    params.append(sub_params)
 
-    classifiers_name = ["KNeighborsClassifier", "DecisionTreeClassifier", "MLPClassifier_sgd_solver",
-                        "MLPClassifier_adam_solver"]
+    # MLPClassifier one layer adam_solver
+    sub_params = []
+    for nodes in range(2, 20, 3):
+        sub_params.append({"solver": 'adam', "activation": 'logistic', "max_iter": 1000, "hidden_layer_sizes": (nodes,),
+                           "verbose": False, "learning_rate_init": 0.1, "tol": 0., "early_stopping": True})
+    params.append(sub_params)
+
+    # MLPClassifier two layers sgd_solver
+    sub_params = []
+    for nodes in range(2, 20, 3):
+        sub_params.append(
+            {"solver": 'sgd', "activation": 'logistic', "max_iter": 1000, "hidden_layer_sizes": (nodes, 5),
+             "verbose": False, "learning_rate_init": 0.1, "tol": 0., "early_stopping": True})
+    params.append(sub_params)
+
+    # MLPClassifier two layers adam_solver
+    sub_params = []
+    for nodes in range(2, 20, 3):
+        sub_params.append(
+            {"solver": 'adam', "activation": 'logistic', "max_iter": 1000, "hidden_layer_sizes": (nodes, 5),
+             "verbose": False, "learning_rate_init": 0.1, "tol": 0., "early_stopping": True})
+    params.append(sub_params)
+
+    classifiers_name = ["KNeighborsClassifier", "DecisionTreeClassifier", "MLPClassifier one layer sgd_solver",
+                        "MLPClassifier one layer adam_solver",
+                        "MLPClassifier two layers, second layer 5 nodes, sgd_solver",
+                        "MLPClassifier two layers, second layer 5 nodes, adam_solver"]
 
     kf = RepeatedKFold(n_splits=5, n_repeats=10, random_state=None)
 
@@ -45,28 +74,24 @@ def main() -> None:
         x = preprocessing.normalize(raw['data'])
         y = raw['target']
 
-        # k_neigh = KNeighborsClassifier()
-        # param_grid = dict(n_neighbors=[1, 5, 15, 25])
-        # grid = GridSearchCV(k_neigh, param_grid, cv=5, n_jobs=10, scoring='accuracy')
-        #
-        # grid.fit(x, y)
-        # print(grid.cv_results_['mean_test_score'])
-
-        for sub_classifiers, name in zip(classifiers, classifiers_name):
+        for classifier, name, sub_param in zip(classifiers, classifiers_name, params):
             scores = []
-            for classifier in sub_classifiers:
+            for param in sub_param:
                 local_scores = []
                 for train_index, test_index in kf.split(x):
                     x_train, x_test = x[train_index], x[test_index]
                     y_train, y_test = y[train_index], y[test_index]
 
-                    classifier.fit(x_train, y_train)
+                    model = classifier(**param)
+                    model.fit(x_train, y_train)
 
-                    predictions = classifier.predict(x_test)
+                    predictions = model.predict(x_test)
                     local_scores.append(accuracy_score(y_test, predictions))
                 scores.append(np.mean(np.array(local_scores)))
-            mean = np.mean(np.array(scores))
-            print("{} mean is {}".format(name, mean))
+            np_arr = np.array(scores)
+            mean = np.mean(np_arr)
+            standard_deviation = np.std(np_arr, ddof=1)
+            print("{} mean is {} with {} as standard deviation".format(name, mean, standard_deviation))
 
 
 if __name__ == '__main__':
