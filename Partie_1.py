e0e84bf0cbeb7075516e79d04e740bcc68273151
José Pinto <jose.ferropinto@epfl.ch>
2018-05-28 10:46:26 +0200

    UPDATE: Best practice

--- Partie_1.py
+++ Partie_1.py
@@ -1,26 +1,32 @@
 from sklearn.cluster import KMeans
 import numpy as np
 
-titanic_data = np.loadtxt('titanic.dat', delimiter=',', skiprows=8)
 
-means = []  # mean value for each K
+def main() -> None:
+    titanic_data = np.loadtxt('titanic.dat', delimiter=',', skiprows=8)
 
-for i in range(1, 20):  # each K
-    KM = KMeans(n_clusters=i, random_state=0).fit(titanic_data)
+    means = []  # mean value for each K
 
-    labels = KM.labels_
-    clusters_distances = KM.transform(titanic_data)
-    mean_distances = []  # mean distances for each cluster and its points
+    for i in range(1, 20):  # each K
+        KM = KMeans(n_clusters=i, random_state=0).fit(titanic_data)
 
-    for j in range(i):  # index of each cluster
-        n_points = 0
-        result = 0
-        for k in range(len(titanic_data)):  # index of each data point
-            if (labels[k] == j):
-                n_points += 1
-                result += clusters_distances[k][j]
-        mean_distances.append(result / n_points)
+        labels = KM.labels_
+        clusters_distances = KM.transform(titanic_data)
+        mean_distances = []  # mean distances for each cluster and its points
 
-    means.append(np.mean(mean_distances))
+        for j in range(i):  # index of each cluster
+            n_points = 0
+            result = 0
+            for k in range(len(titanic_data)):  # index of each data point
+                if (labels[k] == j):
+                    n_points += 1
+                    result += clusters_distances[k][j]
+            mean_distances.append(result / n_points)
 
-print(means)
+        means.append(np.mean(mean_distances))
+
+    print(means)
+
+
+if __name__ == '__main__':
+    main()
