2d0c3ef7cbf62e39f3c73c99c6f8f23be99b2c2b
José Pinto <jose.ferropinto@epfl.ch>
2018-05-28 17:33:01 +0200

    UPDATE: predictions compared

--- Partie_2.py
+++ Partie_2.py
@@ -1,11 +1,13 @@
 # http://scikit-learn.org/stable/datasets/index.html#breast-cancer-wisconsin-diagnostic-database
 
 from sklearn import datasets
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import RepeatedKFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn import preprocessing
+import numpy as np
 
 
 def main() -> None:
@@ -13,15 +15,19 @@ def main() -> None:
     breast_data = preprocessing.normalize(breast_cancer['data'])
     breast_target = breast_cancer['target']
 
+    print(breast_cancer)
+
     wine = datasets.load_wine()
     wine_data = wine['data']
     wine_target = wine['target']
 
     kf = RepeatedKFold(n_splits=5, n_repeats=10, random_state=None)
 
+    n_neigh_scores = []
     for n_neighbors in range(1, 11):
+        local_scores = []
         for train_index, test_index in kf.split(breast_data):
-            print("Train:", train_index, "Validation:", test_index)
+            # print("Train:", train_index, "Validation:", test_index)
             X_train, X_test = breast_data[train_index], breast_data[test_index]
             y_train, y_test = breast_target[train_index], breast_target[test_index]
 
@@ -29,7 +35,12 @@ def main() -> None:
             k_neigh.fit(X_train, y_train)
 
             predictions = k_neigh.predict(X_test)
-            print(predictions)
+            local_scores.append(accuracy_score(y_test, predictions))
+        print(local_scores)
+        n_neigh_scores.append(np.mean(np.array(local_scores)))
+    print(n_neigh_scores)
+    n_neigh_mean = np.mean(np.array(n_neigh_scores))
+    print("n_neigh mean : {}".format(n_neigh_mean))
 
     pass
 
